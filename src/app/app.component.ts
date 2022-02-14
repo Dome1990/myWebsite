5f9adb9b95ec6bb91fc44ae93c761af29e74833a
Dominic Gerth <d.gerth1990@googlemail.com>
2022-02-14 22:28:17 +0100

    addet images and started slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myWebsite';
+
+export class AppComponent implements OnInit{
+
+  images = [
+    'bird.jpg',
+    'finch.jpg',
+    'northern-hawk-owl.jpg',
+    'water.jpg'
+  ];
+
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage(){
+
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(()=>{
+        this.showImage = true;
+      },1)
+    }, 8000)
+
+  }
+
 }
