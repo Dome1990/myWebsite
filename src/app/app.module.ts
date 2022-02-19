a04211d4fac994014299e1555415858646a9c594
Dominic Gerth <d.gerth1990@googlemail.com>
2022-02-19 17:56:02 +0100

    my skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FirstSectionComponent } from './first-section/first-section.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FirstSectionComponent
+    FirstSectionComponent,
+    MySkillsComponent
   ],
   imports: [
     BrowserModule,
