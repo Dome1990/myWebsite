8dcb57c8d2dd5537e64f705bfdb4d716acebdca6
Dominic <d.gerth1990@googlemail.com>
2022-03-18 11:17:08 +0100

    addet scroll to top to router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,19 @@ export class AppComponent{
   currentRoute = '';
   constructor(public router: Router) { }
 
+  onActivate() {
+    // window.scroll(0,0);
+ 
+    window.scroll({ 
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth' 
+     });
+ 
+     //or document.body.scrollTop = 0;
+     //or document.querySelector('body').scrollTo(0,0)
+ }
+
   images = [
     'bird.jpg',
     'finch.jpg',
