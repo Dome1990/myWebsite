489a9266b9e6364d8cc195752079ffd21c00ec32
Dominic <d.gerth1990@googlemail.com>
2022-03-31 19:41:09 +0200

    animated about me component when in viewport

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -10,6 +10,46 @@ export class AboutMeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  isMySkillsScrolledIntoView: boolean | undefined;
+
+  @ViewChild('aboutMe', { static: false })
+  private aboutMe!: ElementRef<HTMLDivElement>;
+  @ViewChild('motivation1', { static: false })
+  private motivation1!: ElementRef<HTMLDivElement>;
+  @ViewChild('motivation2', { static: false })
+  private motivation2!: ElementRef<HTMLDivElement>;
+  @ViewChild('motivation3', { static: false })
+  private motivation3!: ElementRef<HTMLDivElement>;
+  @ViewChild('motivation4', { static: false })
+  private motivation4!: ElementRef<HTMLDivElement>;
+
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView() {
+    const skill = [
+      this.aboutMe,
+      this.motivation1,
+      this.motivation2,
+      this.motivation3,
+      this.motivation4
+    ];
+    this.checkElement(skill);
+  }
+
+  checkElement(skill: any) {
+    for (let i = 0; i < skill.length; i++) {
+      if (skill[i]) {
+        const rect = skill[i].nativeElement.getBoundingClientRect();
+        const topShown = rect.top >= 0;
+        const bottomShown = rect.bottom <= window.innerHeight;
+        this.isMySkillsScrolledIntoView = topShown && bottomShown;
+        if (this.isMySkillsScrolledIntoView) {
+          skill[i].nativeElement.classList.add('seen');
+        }
+      }
+    }
   }
 
 }
